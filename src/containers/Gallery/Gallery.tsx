5d4f4cdfe9cfe9a1347eb2ed2f58f4d433aaaee1
mmusa1994 <muhamed.musa1994@gmail.com>
2023-07-26 19:34:07 +0200

    instagram section desktop

--- src/containers/Gallery/Gallery.tsx
+++ src/containers/Gallery/Gallery.tsx
@@ -9,8 +9,11 @@ interface GalleryProps {
 
 const Gallery: React.FC<GalleryProps> = ({ pictures }) => {
   return (
-    <div>
-      <div className="flex items-center gap-4 w-full max-w-[1440px] overflow-scroll custom-scrollbar">
+    <div className="flex flex-col items-center my-10 gap-5">
+      <div className="chronicle-display font-extrabold text-[36px] leading-[44px] w-[274px] h-[44px] text-primary-dark">
+        <span className="font-thin">#</span>cleanwithbiom
+      </div>
+      <div className="flex items-center gap-6 w-full max-w-[1440px] overflow-scroll custom-scrollbar">
         {pictures.map((picture, index) => (
           <div key={index} className={index % 2 === 1 ? "mt-10" : ""}>
             <Card image={picture} isEven={index % 2 === 0} />
@@ -18,7 +21,7 @@ const Gallery: React.FC<GalleryProps> = ({ pictures }) => {
         ))}
       </div>
       <Button
-        className="brandon-grotesque-bold font-bold text-[13px] tracking-widest uppercase w-[277px] h-[50px]"
+        className="brandon-grotesque-bold font-bold text-[13px] tracking-widest uppercase w-[277px] h-[50px] mt-5"
         variant="tertiary"
         text="Follow us on instagram"
         icon={
