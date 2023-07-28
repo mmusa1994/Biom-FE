27c8154726be22a75a036866586f68c743bb912d
mmusa1994 <muhamed.musa1994@gmail.com>
2023-07-28 09:45:05 +0200

    video width

--- src/containers/Video/Video.tsx
+++ src/containers/Video/Video.tsx
@@ -10,7 +10,7 @@ const VideoSection: React.FC<TVideoSection> = ({
   video,
 }) => {
   return (
-    <div className="relative h-max w-full 2xl:w-[1390px] xl:h-[675px] xl:rounded-[14px] overflow-hidden mt-10">
+    <div className="relative h-max w-full max-w-[1390px] xl:h-[675px] xl:rounded-[14px] overflow-hidden mt-10">
       <div className="absolute flex flex-col items-center justify-center w-full z-10 bg-green-300 bg-opacity-10 h-full">
         <h3 className="font-normal text-xs md:text-base md:leading-[32px] font-brandon-grotesque md:tracking-widest text-sand uppercase">
           {title}
@@ -29,7 +29,7 @@ const VideoSection: React.FC<TVideoSection> = ({
       </div>
 
       <ProgressiveVideo
-        className=""
+        className="w-full h-full"
         src={video}
         preload="metadata"
         loop
